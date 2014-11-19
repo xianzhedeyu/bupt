da9c644abdc74ec65d135af6952564c35d632bae
zhao xiaosen <zhaoxs10@gmail.com>
2014-11-19 23:05:19 +0800

    thread pool

--- asm/asm_rtsp_r8_msg_process.h
+++ asm/asm_rtsp_r8_msg_process.h
@@ -50,8 +50,33 @@ typedef struct _R8_SETUP_RES
 	S7_SS ss;
 	AS_INFO as;
 }R8_SETUP_RES;
+//asm发给sm的teardown消息
+typedef struct _R8_TEARDOWN_MSG
+{
+    char as_ip[40];
+    int as_port;
+    int cseq;
+    char require[256];
+    int reason;
+    INT64 session;
+    char ondemandsessionid[128];
+}R8_TEARDOWN_MSG;
+//as发给asm的teardown response
+typedef struct _R8_TEARDOWN_RES
+{
+    int err_code;
+    int cseq;
+    INT64 session;
+    char ondemandsessionid[128];
+}R8_TEARDOWN_RES;
 
 //创建asm向as发送的setup消息
 int rtsp_r8_setup_msg_encode(R8_SETUP_MSG msg, char *setup_msg);
 //解析as向asm发送的setup response消息
 int rtsp_r8_setup_res_parse(char *setup_res_msg, R8_SETUP_RES *res);
+//创建asm向as发送的teardown消息
+int rtsp_r8_teardown_msg_encode(R8_TEARDOWN_MSG msg, char* teardown_msg);
+//解析as发给asm的teardown response消息
+int rtsp_r8_teardown_res_parse(char* teardown_res_msg, R8_TEARDOWN_RES* res);
+
+#endif // ASM_RTSP_R8_MSG_PROCESS_H_INCLUDED
---
+++ thread_pool.h
@@ -0,0 +1,24 @@
+#ifndef THREAD_POOL_H
+#define THREAD_POOL_H
+
+#include <pthread.h>
+
+typedef struct tpool_work {
+    void* (*routine)(void*);
+    void *arg;
+    struct tpool_work *next;
+}tpool_work_t;
+
+typedef struct tpool {
+    int shutdown;
+    int max_thr_num;
+    pthread_t *thr_id;
+    tpool_work_t *queue_head;
+    pthread_mutex_t queue_lock;
+    pthread_cond_t queue_ready;
+}tpool_t;
+
+int tpool_create(int max_thr_num);
+void tpool_destroy();
+int tpool_add_work(void*(*routine)(void*), void *arg);
+#endif
