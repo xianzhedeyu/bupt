dd1f850168248fa5474f870fbf35e9dc626f1b54
xianzhedeyu <zhaoxs10@gmail.com>
2014-12-06 11:15:58 +0800

    12-06

--- asm/asm_rtsp_s7_msg_process.h
+++ asm/asm_rtsp_s7_msg_process.h
@@ -15,8 +15,8 @@
 
 typedef struct _AS_INFO
 {
-	char as_ip[40];
-	int as_port;
+	char ip[40];
+	int downPort;
 }AS_INFO;
 
 typedef struct _S7_SS
@@ -24,6 +24,7 @@ typedef struct _S7_SS
 	char client[128];
 	char destination[40];
 	int client_port;
+    INT64 bandwidth;
 }S7_SS;
 
 typedef struct _S7_SETUP_MSG
@@ -44,16 +45,64 @@ typedef struct _S7_SETUP_RES
 {
 	int err_code;
 	int cseq;
+    char protocol[10];
 	INT64 session;
 	char ondemand_session_id[128];
 	S7_SS ss;
 	AS_INFO as;
+    INT64 stream_handle;
 }S7_SETUP_RES;
+
+typedef struct _S7_TEARDOWN_MSG
+{
+char asm_ip[40];
+int asm_port;
+int cseq;
+char require[256];
+int reason;
+INT64 session;
+char ondemandsessionid[128];
+}S7_TEARDOWN_MSG;
+
+typedef struct _S7_TEARDOWN_RES
+{
+int err_code;
+int cseq;
+INT64 session;
+char ondemandsessionid[128];
+}S7_TEARDOWN_RES;
+
+typedef struct _S7_ANNOUNCE_MSG
+{
+char sm_ip[40];
+int sm_port;
+int cseq;
+char require[128];
+INT64 session;
+int notice;
+char event_date[30];
+char ondemandsessionid[128];
+}S7_ANNOUNCE_MSG;
+
+typedef struct _S7_ANNOUNCE_RES
+{
+int err_code;
+int cseq;
+INT64 session;
+char ondemandsessionid[128];
+}S7_ANNOUNCE_RES;
 
 //创建asm向sm发送的setup response消息
 int rtsp_s7_setup_res_encode(S7_SETUP_RES resmsg,char *setup_res_msg);
 //解析sm发送给asm的setup消息
 int rtsp_s7_setup_msg_parse(char *setup_msg, S7_SETUP_MSG msg);
-
+//创建asm向sm发送的teardown response消息
+int rtsp_s7_teardown_res_encode(S7_TEARDOWN_RES res, char* teardown_res);
+//解析sm发送给asm的teardown消息
+int rtsp_s7_teardown_msg_parse(char* teardown_msg, S7_TEARDOWN_MSG msg);
+//创建asm发给sm的announce消息
+int rtsp_s7_announce_msg_encode(S7_ANNOUNCE_MSG msg, char* announce_msg);
+//解析sm发给asm的announce response
+int rtsp_s7_announce_res_parse(char* announce_res, S7_ANNOUNCE_RES res);
 #endif
 
